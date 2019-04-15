3fae2454bc52d103d6309dbfe7b6ab1cfc7c0b66
Joli Holmes <jh111@ad.rice.edu>
2019-04-15 11:14:44 -0500

    updated setup file

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "utils",
+    name = "utilsPy",
     version = "0.0.1",
     author = "Joli Holmes",
     author_email = "jh111@rice.edu",
     description = "Python package with basic utility functions",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "XXX",
+    url = "https://github.com/holmesjoli/utilsPy",
     packages = setuptools.find_packages(),
     install_requires = [
         'pyyaml==5.1'
