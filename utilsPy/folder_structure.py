54672da53797973aa343b39d3164f70fbc5e534e
Joli Holmes <jh111@ad.rice.edu>
2019-04-15 09:53:34 -0500

    reorganized the order of functions

--- utilsPy/folder_structure.py
+++ utilsPy/folder_structure.py
@@ -1,15 +1,24 @@
 import os
 
-def remove_files(fls, pth = None):
+def create_dirs(dirs):
+    """
+    Creates a file structure
+    :param dirs: the directories to remove
+    :type dirs: list
+    """
+    
+    [os.mkdir(d) for d in dirs if not os.path.isdir(dirs)]
+
+def remove_files(fls):
     """
     Removes files from the current working directory
     :param fls: the files to remove
     :type fls: list
     """
 
-    [os.remove(d) for d in fls if d in os.listdir(os.getcwd())]
+    [os.remove(d) for d in fls if d in os.listdir(os.path.dirname(d))]
 
-def remove_dirs(dirs, pth = None):
+def remove_dirs(dirs):
     """
     Removes directories from the current working directory
     :param dirs: the directories to remove
@@ -17,13 +26,3 @@ def remove_dirs(dirs, pth = None):
     """
 
     [os.rmdir(d) for d in dirs if d in os.listdir(os.getcwd())]
-
-def create_dirs(dirs, pth = None):
-    """
-    Creates a file structure
-    :param dirs: the directories to remove
-    :type dirs: list
-    """
-    
-    [os.mkdir(d) for d in dirs if not os.path.isdir(os.getcwd())]
-
