2489da21cc8bcd3e1e0f8b5801eb95be323bce3a
Joli Holmes <jh111@ad.rice.edu>
2019-04-15 11:21:01 -0500

    redid spacing

--- utilsPy/tests/test_config.py
+++ utilsPy/tests/test_config.py
@@ -11,15 +11,13 @@ class config_TestClass(unittest.TestCase):
         self.json_pth = "./tests/test.json"
 
     def test_read_yaml(self):
+        
         cu_yaml = read_yaml(self.yaml_pth)
-       
         self.assertTrue(set(cu_yaml.keys()).issubset(set(["test1", "test2"])))
         self.assertTrue(set(cu_yaml["test1"].keys()).issubset(set(["test_var1", "test_var2"])))
         
     def test_read_config(self):
-        cu_json = read_json(self.json_pth)
 
+        cu_json = read_json(self.json_pth)
         self.assertTrue(set(cu_json.keys()).issubset(set(["test1", "test2"])))
         self.assertTrue(set(cu_json["test1"].keys()).issubset(set(["test_var1", "test_var2"])))
-
-        
