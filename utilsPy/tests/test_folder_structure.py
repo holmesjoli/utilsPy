fbc7a01b28748b3029c72f92af819ab7f41dfcd4
Joli Holmes <jh111@ad.rice.edu>
2019-04-15 09:58:39 -0500

    initial commit

---
+++ utilsPy/tests/test_folder_structure.py
@@ -0,0 +1,9 @@
+import unittest
+
+from utilsPy import create_dirs, remove_files, remove_dirs
+
+class folderStructureTestClass(unittest.TestCase):
+ 
+    def __init__(self, *args, **kwargs):
+
+        super(folderStructureTestClass, self).__init__(*args, **kwargs)
